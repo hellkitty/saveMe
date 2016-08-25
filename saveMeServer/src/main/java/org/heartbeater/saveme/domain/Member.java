84afb6bdae16c0d70e8be82cf6b0ffce0ca428e3
HellKitty <i_b@naver.com>
2016-08-25 21:05:42 +0900

    build test comit

---
+++ saveMeServer/src/main/java/org/heartbeater/saveme/domain/Member.java
@@ -0,0 +1,13 @@
+package org.heartbeater.saveme.domain;
+
+public class Member {
+
+	private String memberName;
+	private int memberPhoneNumber; 
+	private int memberAge;
+	private String memberAddress;
+	private int emergencyPhoneNumber1; 
+	private boolean canCpr;
+	
+	
+}
